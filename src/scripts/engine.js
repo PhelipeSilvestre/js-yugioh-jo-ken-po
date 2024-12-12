a57ae0cc7680627521f58366ceb8afecc9231761
phelipeSilvestre <99892687+PhelipeSilvestre@users.noreply.github.com>
2024-12-12 12:03:51 -0300

    implementando o removeAllCards

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -13,15 +13,17 @@ const state = {
     player: document.getElementById("player-field-card"),
     computer: document.getElementById("computer-field-card"),
   },
+  playerSides: {
+    player1: "player-cards",
+    player1Box: document.querySelector("#player-cards"),
+    computer: "computer-cards",
+    computerBox: document.querySelector("#computer-cards")
+  },
   actions: {
     button: document.getElementById("next-duel"),
   },
 };
 
-const playerSides = {
-  player1: "player-cards",
-  computer: "computer-cards",
-};
 
 const cardData = [
   {
@@ -73,6 +75,32 @@ async function createCardImage(idCard, fieldSide) {
   }
 
   return cardImage;
+}
+
+async function setCardsField(cardId) {
+  await removeAllCardsImages();
+
+  let computerCardId = await getRandomCardId();
+
+  state.fieldCards.player.style.display = "block";
+  state.fieldCards.computer.style.display = "block";
+
+  state.fieldCards.player.src = cardData[cardId].img;
+  state.fieldCards.computer.src = cardData[computerCardId].img;
+
+  let duelResults = await checkDuelResults(cardId, computerCardId);
+
+  await updateScore();
+  await drawButton(duelResults);
+}
+
+async function removeAllCardsImages(){
+  let { computerBox, player1Box } = state.playerSides;
+  let imgElements = computerBox.querySelectorAll("img");
+  imgElements.forEach((img) => img.remove());
+
+  imgElements = player1Box.querySelectorAll("img");
+  imgElements.forEach((img) => img.remove());
 }
 
 async function drawSelectedCard(index) {
